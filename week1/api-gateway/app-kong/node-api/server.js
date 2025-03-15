d4eb40a8a103ba0a56460255f6f20cbc1ceac977
pappukrs <digitalpappu101@gmail.com>
2025-03-15 22:22:36 +0530

    api gateway

---
+++ week1/api-gateway/app-kong/node-api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Hello from Kong API Gateway!" });
+});
+
+app.listen(3000, () => console.log("Node API running on port 3000"));
